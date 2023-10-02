03f7aa17d7eaed829c92e3b47558a437e14e2651
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-10-02 20:24:26 +0700

    update projects section

--- src/app/components/ProjectsSection.js
+++ src/app/components/ProjectsSection.js
@@ -1,20 +1,44 @@
+import Image1 from "@public/image1.png";
+import Image from "next/image";
+
 function ProjectCard(project) {
-  return <div>project {project.label}</div>;
+  return (
+    <div className="group relative">
+      <h1 className="hidden group-hover:inline absolute bottom-4 left-6 text-xl">project {project.label}</h1>
+      <Image src={project.imageUrl} alt={project.label} />
+    </div>
+  );
 }
 
 function ProjectsSection() {
   const projects = [
     {
       label: "Project A",
+      imageUrl: Image1,
     },
     {
       label: "Project B",
+      imageUrl: Image1,
+    },
+    {
+      label: "Project B",
+      imageUrl: Image1,
+    },
+    {
+      label: "Project B",
+      imageUrl: Image1,
+    },
+    {
+      label: "Project B",
+      imageUrl: Image1,
     },
   ];
   return (
     <div className="text-black px-12">
       <h1 className="text-3xl font-bold py-4">Latest Projects</h1>
-      {projects.map((project) => ProjectCard(project))}
+      <div className="grid grid-cols-3 gap-6">
+        {projects.map((project) => ProjectCard(project))}
+      </div>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import HeroSection from "./components/HeroSection";
 import ProjectsSection from "./components/ProjectsSection";
+import FooterSection from "./components/FooterSection";
 
 export default function Home() {
+  const headerLinkStyle = "hover:underline hover:cursor-pointer underline-offset-2 decoration-orange-500 decoration-2";
   return (
     <main className="realtive flex min-h-screen flex-col bg-white">
-      <header className="absolute top-0 flex flex-row gap-3 text-black px-12 py-4">
-        <Link href={"#"}>About Me</Link>
-        <Link href={"#"}>Portofolio</Link>
-        <Link href={"#"}>Corporation</Link>
+      <header className="z-10 absolute top-0 flex flex-row gap-3 text-black px-12 py-4">
+        <Link href={"#"} className={headerLinkStyle}>About Me</Link>
+        <Link href={"#"} className={headerLinkStyle}>Portofolio</Link>
+        <Link href={"#"} className={headerLinkStyle}>Corporation</Link>
       </header>
       <HeroSection />
       <ProjectsSection />
+      <FooterSection />
     </main>
   );
 }
